5cbd85cce8140c6a33f743ab49f9f1c84e6c8982
kenjara <kenjara@hotmail.com>
2020-01-28 12:52:59 +0000

    tile identification

--- vs/beta-tester-games/dungeon-tile/dungeon-tile.cpp
+++ vs/beta-tester-games/dungeon-tile/dungeon-tile.cpp
@@ -43,11 +43,46 @@ const uint8_t sprite_width = 16;
 
 point player_location = point(32, 20);
 
+const uint32_t tilemap_width = 32;
+
+const uint32_t tilemap_height = 16;
+
 tilemap world((uint8_t*)layer_world, nullptr, size(32, 16), nullptr);
 
 std::vector<rect> bounding_rectangles = {rect(0,0,  32 * 16, 16), rect(0, 0, 16, 20 * 16), rect(5 * 16, 3 * 16,2 * 16, 2 * 16), rect(5 * 16, 11 * 16, 2 * 16, 2 * 16) };
 
+std::string tile_name;
 
+uint16_t get_tile_from_point(const point& point, uint8_t tile_size, uint8_t tile_map_width)
+{
+    uint16_t horizontal_location = point.x / tile_size;
+
+    if (point.x % tile_size > 0)
+    {
+        horizontal_location += 1;
+    }
+
+    uint16_t vertical_location = (point.y / tile_size) * tile_map_width;
+
+    if (vertical_location % tile_size > 0)
+    {
+        vertical_location += 1;
+    }
+
+    const uint16_t array_location = horizontal_location + vertical_location - 1;
+
+    return array_location;
+}
+
+bool is_point_in_rect(const point& object_origin, std::vector<rect>::value_type bounding_rectangle)
+{
+    if (object_origin.x + sprite_width >= bounding_rectangle.x && object_origin.x <= bounding_rectangle.x + bounding_rectangle.w && object_origin.y + sprite_width > bounding_rectangle.y&& object_origin.y < bounding_rectangle.y + bounding_rectangle.h)
+    {
+        return true;
+    }
+
+    return false;
+}
 
 ///////////////////////////////////////////////////////////////////////////
 //
@@ -61,6 +96,10 @@ void init() {
     fb.sprites = spritesheet::load(packed_data);
 
     world.sprites = fb.sprites;
+
+   //auto tile_index = get_tile_from_point(point(0, 2), 16, tilemap_width);
+
+	
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -95,7 +134,11 @@ void render(uint32_t time) {
 
     fb.sprite(player_sprite, player_location, point(0, 0), vec2(2, 2));
 
+    fb.text(tile_name, &minimal_font[0][0], point(0, 0));
+
 }
+
+
 
 ///////////////////////////////////////////////////////////////////////////
 //
@@ -127,19 +170,35 @@ void update(uint32_t time) {
 
     bool move_ok = true;
 
-    for (auto bounding_rectangle : bounding_rectangles)
+    for (const auto bounding_rectangle : bounding_rectangles)
     {
-        if (new_player_location.x + sprite_width >= bounding_rectangle.x && new_player_location.x <= bounding_rectangle.x + bounding_rectangle.w && new_player_location.y + sprite_width > bounding_rectangle.y && new_player_location.y < bounding_rectangle.y + bounding_rectangle.h)
+        if (is_point_in_rect(new_player_location, bounding_rectangle))
         {
             move_ok = false;
             break;
         }
-    	
-	    /*if(new_player_location.x + sprite_width >= bounding_rectangle.x && new_player_location.x <= bounding_rectangle.x + bounding_rectangle.w && new_player_location.y  <= bounding_rectangle.y + bounding_rectangle.h && new_player_location.y >= bounding_rectangle.y)
-	    {
-            move_ok = false;
-	    	break;
-	    }*/
+    }
+
+    auto tile_index = get_tile_from_point(new_player_location, 16, tilemap_width);
+
+    auto tile_scanned = layer_world[tile_index];
+
+    tile_name = std::to_string(tile_index);
+
+    tile_name.append(" ");
+	
+    switch (tile_scanned)
+    {
+    case 0:
+        tile_name += "Wall";
+    	break;
+    case 1:
+        tile_name += "Floor";
+    	break;
+    default:
+        tile_name +=
+            "unknown";
+    	break;
     }
 
 	if(move_ok)
