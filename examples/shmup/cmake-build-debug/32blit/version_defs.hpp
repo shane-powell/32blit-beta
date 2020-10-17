a932f599400d481a54428745beb09deb0817a198
shane-powell <kenjara@hotmail.com>
2020-10-17 13:04:46 +0100

    Quadcopter assets

--- examples/shmup/cmake-build-debug/32blit/version_defs.hpp
+++ examples/shmup/cmake-build-debug/32blit/version_defs.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-#define	BLIT_BUILD_DATE "2020-09-02"
+#define	BLIT_BUILD_DATE "2020-09-23"
 #define BLIT_BUILD_VER "DEV"
--- quadcopter/cmake-build-debug/assets.cmake
+++ quadcopter/cmake-build-debug/assets.cmake
@@ -1,9 +1,9 @@
 # Auto Generated File - DO NOT EDIT!
 set(ASSET_DEPENDS
-"/home/kenjara85/github/32blit-beta/powder-keg/assets/spritesheet.png"
+"/home/kenjara85/github/32blit-beta/quadcopter/assets/spritesheet.png"
 )
 
 set(ASSET_OUTPUTS
-"/home/kenjara85/github/32blit-beta/powder-keg/cmake-build-debug/assets.hpp"
-"/home/kenjara85/github/32blit-beta/powder-keg/cmake-build-debug/assets.cpp"
+"/home/kenjara85/github/32blit-beta/quadcopter/cmake-build-debug/assets.hpp"
+"/home/kenjara85/github/32blit-beta/quadcopter/cmake-build-debug/assets.cpp"
 )
